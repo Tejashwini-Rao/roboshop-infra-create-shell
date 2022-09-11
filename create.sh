10dd3386d365fefe274eccf0c663616e832cbcd9
Tejashwini Rao <polepalle.07@gmail.com>
2022-09-11 20:25:26 +0530

    changes

--- create.sh
+++ create.sh
@@ -4,18 +4,18 @@ ZONE_ID="Z005396725ZQYS9AQ6CZX"
 #############################
 
 
-COMPONENT=all
+component=all
 create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t3.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=Yes}]"  \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}, {Key=Monitor,Value=Yes}]"  \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       --iam-instance-profile Name=SecretManager_Role_for_RoboShop_Nodes \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/component/${component}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
 
@@ -25,6 +25,6 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NA
 
 
 for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
-  COMPONENT="${component}-${ENV}"
+  component="${component}-${ENV}"
   create_ec2
 done
